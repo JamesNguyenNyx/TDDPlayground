a075615655e4fb6f94483551275c802592a4ff23
JamesNguyenNyx <jamesnguyen.nyx@gmail.com>
2017-05-19 11:06:51 +0700

    RGR workflow of Unit Test
    Implmented Test Driven Development with RGR workflow.

--- TDDPlayground.playground/Contents.swift
+++ TDDPlayground.playground/Contents.swift
@@ -3,6 +3,74 @@
 import UIKit
 import XCTest
 
-struct Lotus {
+class Lotus {
+    
+    func numberOfVowels(in string: String) -> Int {
+        
+        let vowels: [Character] = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+        var numberOfVowels = 0
+        
+        for character in string.characters {
+            if vowels.contains(character) {
+                numberOfVowels += 1
+            }
+        }
+        
+        return numberOfVowels
+    }
+    
+    func makeHeadline(from string: String) -> String {
+        
+        let words = string.components(separatedBy: " ")
+        var headline = ""
+        
+        for var word in words {
+            let firstCharacter = word.remove(at: word.startIndex)
+            headline += "\(String(firstCharacter).uppercased())\(word) "
+        }
+        
+        headline.remove(at: headline.index(before: headline.endIndex))
+        
+        return headline
+    }
+}
+
+class LotusTest: XCTestCase {
+    
+    var lotus: Lotus!
+    
+    override func setUp() {
+        super.setUp()
+        lotus = Lotus()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testLotus() {
+        XCTAssertNotNil(Lotus())
+    }
+    
+    func testNumberOfVowelsRed() {
+        let string = "NguyenAnTinh"
+        let numberOfVowels = lotus.numberOfVowels(in: string)
+        XCTAssertEqual(numberOfVowels, 6, "Should find 4 vowels in NguyenAnTinh")
+    }
+    
+    func testNumberOfVowelsGreen() {
+        let string = "NguyenAnTinh"
+        let numberOfVowels = lotus.numberOfVowels(in: string)
+        XCTAssertEqual(numberOfVowels, 4, "Should find 4 vowels in NguyenAnTinh")
+    }
+    
+    func testHeadlineWithEachWordStartCapitalGreen() {
+        let input = "this is my name Nguyen An Tinh"
+        let expectedOutput = "This Is My Name Nguyen An Tinh"
+        let headline = lotus.makeHeadline(from: input)
+        XCTAssertEqual(headline, expectedOutput)
+    }
     
 }
+
+LotusTest.defaultTestSuite().run()
